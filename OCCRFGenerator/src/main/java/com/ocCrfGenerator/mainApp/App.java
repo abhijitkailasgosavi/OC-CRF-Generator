c23c91640bba39a1854513b4e95ab8ba19b6da14
abhijitkailasgosavi <abhi.gosavi12@gmail.com>
2017-04-01 11:22:00 +0530

    initial code

---
+++ OCCRFGenerator/src/main/java/com/ocCrfGenerator/mainApp/App.java
@@ -0,0 +1,19 @@
+package com.ocCrfGenerator.mainApp;
+
+import java.util.List;
+
+import com.ocCrfGenerator.details.OcCrf;
+import com.ocCrfGenerator.fileIO.CsvOpeartor;
+import com.ocCrfGenerator.fileIO.ExcelOperator;
+
+public class App 
+{
+	public static void main(String[] args) throws Exception {
+		CsvOpeartor csvOperator =new CsvOpeartor();
+		ExcelOperator excelOperator = new ExcelOperator();
+
+		List<OcCrf> list = csvOperator.csvFileRead("/home/neha/eclipse/sampleTestCRF.csv") ;
+		excelOperator.excelFilewriter("sampletestCRF.xls", list);  
+	}
+
+}
